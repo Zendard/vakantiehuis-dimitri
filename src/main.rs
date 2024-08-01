981a77dcde6b376a71ab041145a9ba6e3835b471
Zendard <thijsmuylaert@hotmail.com>
2024-08-01 21:32:58 +0200

    Add language chooser

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ async fn language_chooser() -> Option<NamedFile> {
         .ok()
 }
 
+#[get("/")]
+async fn index_nl() -> Option<NamedFile> {
+    NamedFile::open("templates/nl/index.html").await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![language_chooser])
+        .mount("/nl", routes![index_nl])
         .mount("/", rocket::fs::FileServer::from("public"))
 }
