8babaddac597c2887e64b1634ee375bc7f0de021
Zendard <thijsmuylaert@hotmail.com>
2025-01-02 15:25:21 +0100

    Make carousel more responsive

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -3,15 +3,27 @@ const main_img = document.querySelector("#carousel>img")
 const img_list = document.querySelector("#carousel>div")
 
 let id
+const SCROLL_MARGIN = 50
 
 function setFocus(index) {
   clearTimeout(id)
   document.getElementById("focus").id = ""
 
   img[index].id = "focus"
-  let scrolloffset = img[index].offsetLeft - img_list.scrollWidth / 3
-  console.log(scrolloffset)
-  img_list.scrollLeft = scrolloffset
+  // let scrolloffset = img[index].offsetLeft - img_list.scrollWidth / 3
+  // console.log(scrolloffset)
+  // img_list.scrollLeft = scrolloffset
+  const childOffsetLeft2 = img[index].offsetLeft + img[index].offsetWidth;
+  const containerScrollLeft2 = img_list.scrollLeft + img_list.offsetWidth;
+
+  // is child behind (left)
+  if (img_list.scrollLeft > img[index].offsetLeft) {
+    img_list.scrollLeft = img[index].offsetLeft - SCROLL_MARGIN;
+  }
+  // is child ahead (right)
+  if (containerScrollLeft2 < childOffsetLeft2) {
+    img_list.scrollLeft += childOffsetLeft2 - containerScrollLeft2 + SCROLL_MARGIN;
+  }
 
   main_img.attributes.src.value = img[index].attributes.src.value
   main_img.attributes.alt.value = img[index].attributes.alt.value
